ccfa1b0eac30deb27d79600767b46a4b907aceed
eatspaint <cef2010@gmail.com>
2016-04-25 19:38:03 -0600

    Remove non-threadsafe code

--- config/initializers/RSpot_extend.rb
+++ config/initializers/RSpot_extend.rb
@@ -4,10 +4,9 @@ end
 
 module Tracks
   def all_tracks!
-    length = self.total
     @all_tracks = self.tracks_cache
     offset = 100
-    while @all_tracks.length != length
+    while @all_tracks.length != self.total
       @all_tracks += self.tracks(offset: offset)
       offset += 100
     end
