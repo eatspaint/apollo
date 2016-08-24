a19863120d834186baaf16642830115f0d8adcd0
eatspaint <cef2010@gmail.com>
2016-08-24 02:11:03 -0600

    More mobile friendly

--- app/assets/javascripts/components/Landing.js.jsx
+++ app/assets/javascripts/components/Landing.js.jsx
@@ -5,7 +5,7 @@ class Landing extends React.Component{
   render() {
     return(
       <div>
-        <div className='landing-header valign-wrapper'>
+        <div className='landing-header valign-wrapper hide-on-small-only'>
           <div className='valign full-width'>
             <p id='landing-welcome' className='landing-welcome center-align'>Welcome to Apollo</p>
             <div className='yellow-hr' style={{width: '45%'}}></div>
